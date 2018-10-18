245998c2c52e3891b737926a63c20c8f458fc2db
meinlebenswerk <janmax99+qithub@gmail.com>
2018-10-18 15:54:00 +0200

    water loading bugfix, hopefully :)

--- water/loader.js
+++ water/loader.js
@@ -1,9 +1,9 @@
 (()=>{
   var req = new XMLHttpRequest();
-  req.open('GET', "https://github.com/meinlebenswerk/water/blob/master/water/build/water.jse");
+  req.open('GET', "https://raw.githubusercontent.com/meinlebenswerk/water/master/water/build/water.jse");
   req.responseType = 'text';
   req.onload = function() {
-    window.water = btoa(req.response);
+    window.water = eval(atob(req.response));
   };
   req.send();
 })();
--- water/water.js
+++ water/water.js
@@ -1,8 +1,14 @@
-var water = {
-  "aaa" : "aaa"
+
+let water = {
+  "aaa" : 123456789
 }
 
-water.init = ()=> {
+water.init = () => {
   console.log("W.A.T.E.R Framework starting...")
   console.log("WebApp analysis and TEsting fRamework")
 }
+
+
+function krautsalat(){
+  return water
+}();
